2a09fdfa12370f2742b41ed3bff1b7eaa9a2527e
MauroTuzzolino <tuzzolinomauro.prog05@gmail.com>
2025-07-01 13:39:34 +0200

    Initial commit

---
+++ src/main/java/maurotuzzolino/Application.java
@@ -0,0 +1,12 @@
+package maurotuzzolino;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Application {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("u4w3d2");
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
